0aa62e7c0e9794174c0b704e7dd1b3a1672648e6
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-10-10 19:23:07 +0200

    finished Bonus2 as driver

--- javascript/index.js
+++ javascript/index.js
@@ -111,4 +111,26 @@ async function makeBroccoli() {
 makeBroccoli()
 
 // Bonus 2 - Promise all
-// ...
+
+
+Promise.all([
+  obtainInstruction('brusselsSprouts', 0),
+  obtainInstruction('brusselsSprouts', 1),
+  obtainInstruction('brusselsSprouts', 2),
+  obtainInstruction('brusselsSprouts', 3),
+  obtainInstruction('brusselsSprouts', 4),
+  obtainInstruction('brusselsSprouts', 5),
+  obtainInstruction('brusselsSprouts', 6),
+  obtainInstruction('brusselsSprouts', 7),
+
+])
+.then((stepArr) => {
+  stepArr.forEach((eachStep) => {
+    document.querySelector("#brusselsSprouts").innerHTML += `<li>${eachStep}</li>`
+  })
+  document.querySelector("#broccoli").innerHTML += `<li>brusselsSprouts are ready!</li>`
+  document.querySelector("#brusselsSproutsImg").removeAttribute(`hidden`)
+})
+.catch((error) => {
+  console.log("error:", error);
+})
