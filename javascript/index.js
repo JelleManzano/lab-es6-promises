caf9c68b8ead95cd584f0a67d91cd11175152fb3
Jose Carlos Sáez Fernández <jcsaezfernandez@gmail.com>
2022-10-10 18:38:48 +0200

    finished iteration 2 as driver with bonus1

--- javascript/index.js
+++ javascript/index.js
@@ -45,20 +45,45 @@ getInstruction("mashedPotatoes", 0, (step0) => {
 }, (error) => console.log(error));
 
 
-/*getInstruction(`mashedPotatoes`, 1, (step1) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`
-})
-getInstruction(`mashedPotatoes`, 2, (step2) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`
-})
-getInstruction(`mashedPotatoes`, 3, (step3) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`
-})
-getInstruction(`mashedPotatoes`, 4, (step4) => {
-  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`
-})*/
-
 // Iteration 2 - using promises
+
+obtainInstruction('steak', 0)
+  .then( (step0) => {
+    document.querySelector("#steak").innerHTML += `<li>${step0}</li>`
+    return obtainInstruction('steak', 1)
+    //  ... Your code here
+  })
+  .then((step1) => {
+    document.querySelector("#steak").innerHTML += `<li>${step1}</li>`
+    return obtainInstruction('steak', 2)
+    
+  })
+  .then ((step2) => {
+    document.querySelector("#steak").innerHTML += `<li>${step2}</li>`
+    return obtainInstruction('steak', 3)
+
+  })
+  .then ((step3) => {
+    document.querySelector("#steak").innerHTML += `<li>${step3}</li>`
+    return obtainInstruction('steak', 4)
+  })
+  .then ((step4) => {
+    document.querySelector("#steak").innerHTML += `<li>${step4}</li>`
+    return obtainInstruction('steak', 5)
+  })
+  .then ((step5) => {
+    document.querySelector("#steak").innerHTML += `<li>${step5}</li>`
+    return obtainInstruction('steak', 6)
+  })
+  .then ((step6) => {
+    document.querySelector("#steak").innerHTML += `<li>${step6}</li>`
+    document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`
+    document.querySelector("#steakImg").removeAttribute(`hidden`)
+  })
+
+
+  // ... Your code here
+
 // ...
 
 // Iteration 3 using async/await
